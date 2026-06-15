f93ec9cd1b52acb6890f68991067e8d6e1d2f234
suhendrimunir-svg <suhendrimunir@gmail.com>
2026-06-15 17:49:38 +0700

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new project details.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,17 +3,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // TODO: Ganti dengan Firebase SDK Config Anda dari Firebase Console
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY_ANDA",
-  authDomain: "PROJECT_ID_ANDA.firebaseapp.com",
-  projectId: "PROJECT_ID_ANDA",
-  storageBucket: "PROJECT_ID_ANDA.appspot.com",
-  messagingSenderId: "SENDER_ID_ANDA",
-  appId: "APP_ID_ANDA"
+  apiKey: "AIzaSyBIwS3TfnomxPq6bw3173OVzwD_gvJ-qsg",
+  authDomain: "sekolahku-a2cf6.firebaseapp.com",
+  projectId: "sekolahku-a2cf6",
+  storageBucket: "sekolahku-a2cf6.firebasestorage.app",
+  messagingSenderId: "285921284769",
+  appId: "1:285921284769:web:f2042b5bd2647d8989d6e0",
+  measurementId: "G-0NB6100FEY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore dan export agar bisa dipakai di file lain
 export const db = getFirestore(app);
